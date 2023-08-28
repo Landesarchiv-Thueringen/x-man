0ec131a4936190016e1a70f9e9323dc9516559b0
Tony Grochow <tony.grochow@la.thueringen.de>
2023-08-28 14:23:58 +0200

    server init

--- backend/server.go
+++ backend/server.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+  fmt.Println("LATh xdomea server is running")
+  router := gin.Default()
+  router.GET("", getDefaultResponse)
+  router.Run("localhost:3000")
+}
+
+func getDefaultResponse(context *gin.Context) {
+  context.String(http.StatusOK, "LATh xdomea server is running")
 }
