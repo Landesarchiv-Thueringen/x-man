5c58acc3f0f8c81b573a9fbc9553b885fec71e5b
Tony Grochow <tony.grochow@la.thueringen.de>
2023-08-29 13:24:11 +0200

    Create temporary copy of new messages.

--- backend/internal/temporarystorage/temporarystorage.go
+++ backend/internal/temporarystorage/temporarystorage.go
@@ -11,24 +11,28 @@ import (
 )
 
 func StoreMessage(messagePath string) {
-	message, err := os.Open(messagePath)
-	if (err) != nil {
-		log.Fatal(err)
-	}
-	defer message.Close()
 	id := xdomea.GetMessageID(messagePath)
 	messageName := filepath.Base(messagePath)
+	// Create temporary directory. The name of the directory ist the message ID.
 	tempDir, err := ioutil.TempDir("", id)
 	if (err) != nil {
 		log.Fatal(err)
 	}
 	defer os.RemoveAll(tempDir)
+	// Open the original message in the transfer directory.
+	message, err := os.Open(messagePath)
+	if (err) != nil {
+		log.Fatal(err)
+	}
+	defer message.Close()
+	// Create a file in the temporary directory.
 	copyPath := path.Join(tempDir, messageName)
 	copy, err := os.Create(copyPath)
 	if (err) != nil {
 		log.Fatal(err)
 	}
 	defer copy.Close()
+	// Copy the message to the new file.
 	_, err = io.Copy(copy, message)
 	if (err) != nil {
 		log.Fatal(err)
