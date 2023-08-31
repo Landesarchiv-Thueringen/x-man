4174c39b292b9e2faa81036e37137a46303607c0
Tony Grochow <tony.grochow@la.thueringen.de>
2023-08-31 14:10:33 +0200

    parser works!

--- backend/internal/xdomea/parser.go
+++ backend/internal/xdomea/parser.go
@@ -6,32 +6,55 @@ import (
 	"lath/xdomea/internal/db"
 	"log"
 	"os"
+
+	"gorm.io/gorm"
 )
 
 type Message0501 struct {
+	gorm.Model
 	XMLName       xml.Name       `xml:"Aussonderung.Anbieteverzeichnis.0501"`
 	MessageHead   MessageHead    `xml:"Kopf"`
 	RecordObjects []RecordObject `xml:"Schriftgutobjekt"`
 }
 
 type MessageHead struct {
+	gorm.Model
 	XMLName   xml.Name `xml:"Kopf"`
 	ProcessID string   `xml:"ProzessID"`
 }
 
 type RecordObject struct {
+	gorm.Model
 	XMLName           xml.Name           `xml:"Schriftgutobjekt"`
 	FileRecordObjects []FileRecordObject `xml:"Akte"`
 }
 
 type FileRecordObject struct {
+	gorm.Model
 	XMLName         xml.Name `xml:"Akte"`
 	GeneralMetadata GeneralMetadata
+	Lifetime        Lifetime
 }
 
 type GeneralMetadata struct {
-	XMLName xml.Name `xml:"AllgemeineMetadaten"`
-	Subject string   `xml:"Betreff"`
+	gorm.Model
+	XMLName  xml.Name `xml:"AllgemeineMetadaten"`
+	Subject  string   `xml:"Betreff"`
+	XdomeaID string   `xml:"Kennzeichen"`
+	FilePlan FilePlan `xml:"Aktenplaneinheit"`
+}
+
+type FilePlan struct {
+	gorm.Model
+	XMLName  xml.Name `xml:"Aktenplaneinheit"`
+	XdomeaID string   `xml:"Kennzeichen"`
+}
+
+type Lifetime struct {
+	gorm.Model
+	XMLName xml.Name `xml:"Laufzeit"`
+	Start   string   `xml:"Beginn"`
+	End     string   `xml:"Ende"`
 }
 
 func ParseMessage(message db.Message) {
@@ -46,6 +69,9 @@ func ParseMessage(message db.Message) {
 		log.Fatal(err)
 	}
 	var messageEl Message0501
-	xml.Unmarshal(xmlBytes, &messageEl)
+	err = xml.Unmarshal(xmlBytes, &messageEl)
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Println(messageEl)
 }
