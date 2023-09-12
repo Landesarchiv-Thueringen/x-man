181549c0158c4b2c79e0efb62285f2bbdd7c4264
Tony Grochow <tony.grochow@la.thueringen.de>
2023-09-12 18:53:23 +0200

    paginator localization

--- new_frontend/src/app/app.module.ts
+++ new_frontend/src/app/app.module.ts
@@ -7,29 +7,32 @@ import { HttpClientModule } from '@angular/common/http';
 import localeDe from '@angular/common/locales/de';
 registerLocaleData(localeDe);
 
-// project
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+// material
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu'; 
-import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule, MatPaginatorIntl} from '@angular/material/paginator';
 import { MatSidenavModule } from '@angular/material/sidenav'; 
 import { MatSortModule} from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTreeModule } from '@angular/material/tree';
-import { Message0501TableComponent } from './message0501-table/message0501-table.component';
-import { Message0503TableComponent } from './message0503-table/message0503-table.component';
-import { MessageViewComponent } from './message-view/message-view.component';
+
+// project
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageMetadataComponent } from './metadata/message-metadata/message-metadata.component';
 import { FileMetadataComponent } from './metadata/file-metadata/file-metadata.component';
 import { InstitutMetadataComponent } from './metadata/institution-metadata/institution-metadata.component';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { Message0501TableComponent } from './message0501-table/message0501-table.component';
+import { Message0503TableComponent } from './message0503-table/message0503-table.component';
+import { MessageViewComponent } from './message-view/message-view.component';
+import { PaginatorDeService } from './utility/localization/paginator-de.service';
 
 @NgModule({
   declarations: [
@@ -61,7 +64,11 @@ import { InstitutMetadataComponent } from './metadata/institution-metadata/insti
     MatToolbarModule,
     ReactiveFormsModule,
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'de' }, DatePipe],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de' },
+    { provide: MatPaginatorIntl, useClass: PaginatorDeService },
+    DatePipe,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
