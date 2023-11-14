24d17a9ad09bc66baaae859776044b03c13a957e
Tony Grochow <tony.grochow@la.thueringen.de>
2023-11-14 19:25:36 +0100

    endpoint fix

--- server/internal/format/verification.go
+++ server/internal/format/verification.go
@@ -1,3 +1,15 @@
 package format
 
+import (
+	"lath/xman/internal/db"
+	"log"
+
+	"github.com/google/uuid"
+)
+
 var BorgEndpoint = "http://localhost:3000/analyse"
+
+func VerifyFileFormats(messageID uuid.UUID) {
+	primaryDocuments, _ := db.GetAllPrimaryDocuments(messageID)
+	log.Println(primaryDocuments)
+}
