34fde4b9ea9ff5382b4122ae764284105b6b4cf3
Christopher Lübbemeier <Christopher.Luebbemeier@la.thueringen.de>
2024-02-13 16:22:35 +0100

    Fixed top bar

--- gui/src/app/app-routing.module.ts
+++ gui/src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import { UsersComponent } from './admin/users/users.component';
 import { ClearingTableComponent } from './clearing/clearing-table/clearing-table.component';
 import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
-import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 import { MessageTreeComponent } from './message/message-tree/message-tree.component';
 import { DocumentMetadataComponent } from './metadata/document-metadata/document-metadata.component';
 import { FileMetadataComponent } from './metadata/file-metadata/file-metadata.component';
@@ -19,54 +18,48 @@ import { ProcessTableComponent } from './process/process-table/process-table.com
 import { isAdmin, isLoggedIn } from './utility/authorization/auth-guards';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   {
-    path: '',
-    component: MainNavigationComponent,
+    path: 'aussonderungen',
+    component: ProcessTableComponent,
+    canActivate: [isLoggedIn],
+  },
+  {
+    path: 'nachricht/:id',
+    component: MessageTreeComponent,
+    canActivate: [isLoggedIn],
     children: [
-      { path: 'login', component: LoginComponent },
-      {
-        path: 'aussonderungen',
-        component: ProcessTableComponent,
-        canActivate: [isLoggedIn],
-      },
-      {
-        path: 'nachricht/:id',
-        component: MessageTreeComponent,
-        canActivate: [isLoggedIn],
-        children: [
-          { path: '', redirectTo: 'details', pathMatch: 'full' },
-          { path: 'details', component: MessageMetadataComponent },
-          { path: 'akte/:id', component: FileMetadataComponent },
-          { path: 'vorgang/:id', component: ProcessMetadataComponent },
-          { path: 'dokument/:id', component: DocumentMetadataComponent },
-          { path: 'formatverifikation', component: PrimaryDocumentsTableComponent },
-        ],
-      },
-      {
-        path: 'steuerungsstelle',
-        component: ClearingTableComponent,
-        canActivate: [isAdmin],
-      },
-      {
-        path: 'administration',
-        component: AdminComponent,
-        canActivate: [isAdmin],
-        children: [
-          { path: '', redirectTo: 'abgebende-stellen', pathMatch: 'full' },
-          { path: 'abgebende-stellen', component: AgenciesComponent },
-          { path: 'bestände', component: CollectionsComponent },
-          { path: 'mitarbeiter', component: UsersComponent },
-          { path: 'prozesse', component: TasksComponent },
-        ],
-      },
-      {
-        path: 'error/:code',
-        component: ErrorComponent,
-      },
-      { path: '', redirectTo: '/aussonderungen', pathMatch: 'full' },
-      { path: '**', redirectTo: '/error/404' },
+      { path: '', redirectTo: 'details', pathMatch: 'full' },
+      { path: 'details', component: MessageMetadataComponent },
+      { path: 'akte/:id', component: FileMetadataComponent },
+      { path: 'vorgang/:id', component: ProcessMetadataComponent },
+      { path: 'dokument/:id', component: DocumentMetadataComponent },
+      { path: 'formatverifikation', component: PrimaryDocumentsTableComponent },
     ],
   },
+  {
+    path: 'steuerungsstelle',
+    component: ClearingTableComponent,
+    canActivate: [isAdmin],
+  },
+  {
+    path: 'administration',
+    component: AdminComponent,
+    canActivate: [isAdmin],
+    children: [
+      { path: '', redirectTo: 'abgebende-stellen', pathMatch: 'full' },
+      { path: 'abgebende-stellen', component: AgenciesComponent },
+      { path: 'bestände', component: CollectionsComponent },
+      { path: 'mitarbeiter', component: UsersComponent },
+      { path: 'prozesse', component: TasksComponent },
+    ],
+  },
+  {
+    path: 'error/:code',
+    component: ErrorComponent,
+  },
+  { path: '', redirectTo: '/aussonderungen', pathMatch: 'full' },
+  { path: '**', redirectTo: '/error/404' },
 ];
 
 @NgModule({
