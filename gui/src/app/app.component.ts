80d44c6539a6cc3e841efb015d88ef8cc22688b5
Christopher Lübbemeier <Christopher.Luebbemeier@la.thueringen.de>
2024-04-11 17:41:22 +0200

    Icons for tree groups

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { MainNavigationComponent } from './core/main-navigation/main-navigation.component';
 
@@ -7,6 +9,20 @@ import { MainNavigationComponent } from './core/main-navigation/main-navigation.
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterModule, MainNavigationComponent],
+  imports: [RouterModule, MainNavigationComponent, MatIconModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'folders',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/folders.svg'),
+    );
+    this.matIconRegistry.addSvgIcon(
+      'articles',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/articles.svg'),
+    );
+  }
+}
