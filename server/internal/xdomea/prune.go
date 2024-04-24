da246d423a4484d3632f3704ecfca31d8b0cdc1d
Tony Grochow <tony.grochow@la.thueringen.de>
2024-04-24 14:24:32 +0200

    Removed unecesarry logs.

--- server/internal/xdomea/prune.go
+++ server/internal/xdomea/prune.go
@@ -3,7 +3,6 @@ package xdomea
 import (
 	"errors"
 	"lath/xman/internal/db"
-	"log"
 	"slices"
 
 	"github.com/beevik/etree"
@@ -31,9 +30,6 @@ func PruneMessage(message db.Message, aip db.ArchivePackage) (string, error) {
 		if recordEl != nil {
 			idEl := recordEl.FindElementPath(idPathXdomea)
 			if idEl != nil {
-				log.Println(rootIDs)
-				log.Println(idEl.Text())
-				log.Println(slices.Contains(rootIDs, idEl.Text()))
 				if !slices.Contains(rootIDs, idEl.Text()) {
 					removedChild := root.RemoveChild(genericRecord)
 					// Should never happen unless the xdomea specification changes.
