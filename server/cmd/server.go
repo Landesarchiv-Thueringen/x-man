f069d63acf4281497f4fc7372428fdbac6ae1992
Christine Träger <67954057+chtraeger@users.noreply.github.com>
2024-05-03 14:19:29 +0200

    Set version to 0.9.0

--- server/cmd/server.go
+++ server/cmd/server.go
@@ -24,8 +24,8 @@ import (
 	"github.com/google/uuid"
 )
 
-const XMAN_MAJOR_VERSION = 1
-const XMAN_MINOR_VERSION = 0
+const XMAN_MAJOR_VERSION = 0
+const XMAN_MINOR_VERSION = 9
 const XMAN_PATCH_VERSION = 0
 
 var XMAN_VERSION = fmt.Sprintf("%d.%d.%d", XMAN_MAJOR_VERSION, XMAN_MINOR_VERSION, XMAN_PATCH_VERSION)
