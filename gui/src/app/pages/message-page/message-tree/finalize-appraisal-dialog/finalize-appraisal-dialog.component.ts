84139960f9d0e4f155862491dc393a2d8c65757c
Christopher Lübbemeier <Christopher.Luebbemeier@la.thueringen.de>
2024-09-12 10:57:17 +0200

    Progress spinner in finalize-appraisal dialog

--- gui/src/app/pages/message-page/message-tree/finalize-appraisal-dialog/finalize-appraisal-dialog.component.ts
+++ gui/src/app/pages/message-page/message-tree/finalize-appraisal-dialog/finalize-appraisal-dialog.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MessageService } from '../../../../services/message.service';
 
 export interface DialogData {
@@ -13,25 +14,23 @@ export interface DialogData {
   templateUrl: './finalize-appraisal-dialog.component.html',
   styleUrls: ['./finalize-appraisal-dialog.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatProgressSpinnerModule],
 })
 export class FinalizeAppraisalDialogComponent {
-  appraisalComplete: boolean;
+  loading = true;
+  appraisalComplete?: boolean;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private dialogRef: MatDialogRef<FinalizeAppraisalDialogComponent>,
     private messageService: MessageService,
   ) {
-    this.appraisalComplete = false;
-    this.messageService.areAllRecordObjectsAppraised(data.processId).subscribe({
-      error: (error: any) => {
-        console.error(error);
-      },
-      next: (appraisalComplete: boolean) => {
+    this.messageService
+      .areAllRecordObjectsAppraised(data.processId)
+      .subscribe((appraisalComplete) => {
+        this.loading = false;
         this.appraisalComplete = appraisalComplete;
-      },
-    });
+      });
   }
 
   sendAppraisalMessage(): void {
