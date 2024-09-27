f03c4857d60625cdbb5ef796a14dac21985fd335
Christopher Lübbemeier <Christopher.Luebbemeier@la.thueringen.de>
2024-09-27 11:12:44 +0200

    Remove obsolete FIXME

--- gui/src/proxy.conf.mjs
+++ gui/src/proxy.conf.mjs
@@ -5,8 +5,6 @@ export default {
     // Forward connection closing to the server. This is relevant for
     // server-sent events, since the server keeps the connection open
     // indefinitely otherwise.
-    //
-    // FIXME: This causes "Error: socket hang up".
     onProxyReq: (proxyRes, req, res) => {
       res.on("close", () => proxyRes.destroy());
     },
