367fcb0a4ece2801a0f0f167acd6813b939d2620
Christopher Lübbemeier <Christopher.Luebbemeier@la.thueringen.de>
2025-03-06 14:48:05 +0100

    Add publish-images.sh

---
+++ scripts/publish-images.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Builds and publishes Docker images.
+#
+# Usage:
+#   - Populate your .env file, especially IMAGE_PREFIX and IMAGE_VERSION.
+#   - Generally, IMAGE_VERSION should be equal the git tag of the exact commit
+#     that is currently checked out. Alternatively, it can be a custom tag like
+#     "dev".
+#   - Run this script.
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source "$SCRIPT_DIR/../.env"
+
+tags=(
+    gui
+    server
+    report
+)
+
+echo "Build and publish"
+for tag in "${tags[@]}"; do
+    echo "    $IMAGE_PREFIX/$tag:$IMAGE_VERSION"
+done
+echo
+read -p "Continue? [y/N] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+    echo Building images...
+    (
+        cd "$SCRIPT_DIR/.."
+        docker compose -f compose.yml build --pull
+    )
+    echo Publishing images...
+    for tag in "${tags[@]}"; do
+        docker push "$IMAGE_PREFIX/$tag:$IMAGE_VERSION"
+    done
+fi
